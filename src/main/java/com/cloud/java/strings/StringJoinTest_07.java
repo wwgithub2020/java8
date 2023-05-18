ff96e871b5760d671fa193ee6da89d9101d21c3b
adminwei <1766990107>
2023-05-18 21:24:26 +0800

    【feat】：字符串拼接-2

--- src/main/java/com/cloud/java/strings/StringJoinTest_07.java
+++ src/main/java/com/cloud/java/strings/StringJoinTest_07.java
@@ -1,7 +1,10 @@
 package com.cloud.java.strings;
 
+import org.junit.Test;
+
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @CreateTime: 2023-05-18  20:11
@@ -16,4 +19,34 @@ public class StringJoinTest_07 {
         System.out.println("拼接结果是：" + joined);
     }
 
+    @Test
+    public void testJoin_01() {
+        String str = assignStr("java","Spring Boot", "Spring Cloud");
+        System.out.println("结果是：" + str);
+    }
+
+    @Test
+    public void testJoin_02() {
+        String joinStr = joinStr("k8s", "spring", "java", "car", "blue");
+        System.out.println("结果是：" + joinStr);
+    }
+
+    /**
+     * 指定位置进行字符串拼接
+     * @param args
+     * @return
+     */
+    public static String assignStr(String... args) {
+        return String.format("key:%s:route:%s", args);
+    }
+
+    /**
+     * 字符串拼接
+     * @param args
+     * @return
+     */
+    public static String joinStr(String... args) {
+        List<String> list = Arrays.stream(args).collect(Collectors.toList());
+        return String.join("-", list);
+    }
 }
